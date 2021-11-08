bbd042b5c17278902aa957975d61d602b4d849ab
Ben Furlani <bfurlani@brautomationinc.com>
2021-11-08 16:41:36 -0500

    Add project files.

---
+++ ExampleCalculator.ViewModel/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace ExampleCalculator.ViewModel
+{
+
+    
+
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private string input;
+
+        public string Input
+        {
+            get { return input; }
+            set { input = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private ICommand inputCommand;
+
+        public ICommand InputCommand
+        {
+            get {
+                if(inputCommand == null)
+                {
+                    inputCommand = new RelayCommand(x => ProccessInput(x));
+                }
+                return inputCommand; }
+            set { inputCommand = value; }
+        }
+
+        private void ProccessInput(object x)
+        {
+            Input += x.ToString();
+        }
+    }
+
+
+
+}
